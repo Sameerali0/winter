9d335336fe6dbd4f4076eb306d974047ea24c69d
Sameerali0 <sameer.ali.professional@gmail.com>
2025-11-27 12:20:17 +0500

    fixed now when player move yeti move player stop yeti speed increase yeti catch player and game over

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ let playerSpeed=3
 let yetiSpeed= 2.5
 let isMoving= false
 let gameOver= false
+let gameStarted= false
 
 document.addEventListener("keydown", (e)=>{
 
         if(e.key === "ArrowRight"){
 
             isMoving=true
+            gameStarted= true
         }
 })
 
@@ -39,17 +41,19 @@ function game(){
         playerX +=playerSpeed
     }
 
-    player.style.left = playerX +"px"
+    player.style.left= playerX +"px"
 
-    if(isMoving){
+    if(gameStarted){
+        if(isMoving){
 
         yetiX += yetiSpeed
 
-    }else{
+        }else{
 
-        yetiX += yetiSpeed * 2
+            yetiX += yetiSpeed * 2
+        }
     }
-    
+
 
     yeti.style.left= yetiX + "px"
 
